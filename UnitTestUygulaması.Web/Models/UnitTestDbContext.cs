def6440636572e7358dcfb4e5102f006f945c695
TayfunSentrk <tayfun.senturk.yahoo.com>
2024-08-15 11:17:40 +0300

    Unit Test İçin Mvc Projesi Oluşturuldu.Database first için EntityFramework kütüphaneleri kuruldu.Sql'de yer alan veritabanı yolu programa entegre edildi

---
+++ UnitTestUygulaması.Web/Models/UnitTestDbContext.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace UnitTestUygulaması.Web.Models
+{
+    public partial class UnitTestDbContext : DbContext
+    {
+        public UnitTestDbContext()
+        {
+        }
+
+        public UnitTestDbContext(DbContextOptions<UnitTestDbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Product> Products { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Data Source=DESKTOP-79PB5GJ\\TAYFUN;Initial Catalog=UnitTestDb;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>(entity =>
+            {
+                entity.ToTable("Product");
+
+                entity.Property(e => e.Color).HasMaxLength(50);
+
+                entity.Property(e => e.Name).HasMaxLength(200);
+
+                entity.Property(e => e.Price).HasColumnType("decimal(18, 2)");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
