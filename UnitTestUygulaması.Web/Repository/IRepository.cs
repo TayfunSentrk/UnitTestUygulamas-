b257e1eef9be66f18b3e5c7578bd6c792fcb5171
TayfunSentrk <tayfun.senturk.yahoo.com>
2024-08-15 11:44:45 +0300

    IRepository Oluşturuldu.Crud İşlemleri için ilgili methodlar yazıldı

---
+++ UnitTestUygulaması.Web/Repository/IRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace UnitTestUygulaması.Web.Repository
+{
+    public interface IRepository<T> where T : class
+
+    {
+        Task<IEnumerable<T>> GetAllAsync(); 
+        Task<T> GetAsync(int id);
+        Task Create(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
